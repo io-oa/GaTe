897081fb0dd8c8a33056475d3d6611fcd41664cf
kaspeo <119288675+kaspeo@users.noreply.github.com>
2024-11-03 21:13:07 +0100

    Menu was added

---
+++ GUI/Scripts/settings_menu.gd
@@ -0,0 +1,14 @@
+class_name SettingsMenu
+extends Control
+
+@onready var exit_settings_button: Button = $MarginContainer/VBoxContainer/Exit_Settings_Button
+
+signal exit_settings_menu
+
+func _ready() -> void:
+	exit_settings_button.button_down.connect(on_exit_settings_pressed)
+	set_process(false)
+
+func on_exit_settings_pressed() -> void:
+	exit_settings_menu.emit()
+	set_process(false)
