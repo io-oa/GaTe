7a11dc7956c3629c7320ba57213c436c2f3f5d11
kaspeo <119288675+kaspeo@users.noreply.github.com>
2024-11-06 21:41:39 +0100

    Menu was modified
    -Added audio settings and sound
    -Fixed menu controls

---
+++ GUI/Scripts/audio_settings.gd
@@ -0,0 +1,33 @@
+extends Control
+
+@onready var audio_name_lbl: Label = $HBoxContainer/Audio_Name_Lbl
+@onready var audio_num_lbl: Label = $HBoxContainer/Audio_Num_Lbl
+@onready var h_slider: HSlider = $HBoxContainer/HSlider
+
+@export_enum("Master", "Music", "Sfx") var bus_name : String
+
+var bus_index: int = 0
+
+func _ready() -> void:
+	h_slider.value_changed.connect(on_value_changed)
+	
+	get_bus_name_by_index()  # Ensure you get the bus index before using it
+	set_slider_value()  # Initialize the slider value
+	set_name_label_text()
+
+func set_name_label_text() -> void:
+	audio_name_lbl.text = str(bus_name) + " Volume"
+
+func set_audio_num_label_text() -> void:
+	audio_num_lbl.text = str(h_slider.value * 100) + "%"
+
+func get_bus_name_by_index() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+
+func set_slider_value() -> void:
+	h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	set_audio_num_label_text()  # Ensure the label is updated when the slider is set
+
+func on_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	set_audio_num_label_text()
--- GUI/Scripts/hotkey_rebind_button.gd
+++ GUI/Scripts/hotkey_rebind_button.gd
@@ -5,12 +5,13 @@ extends Control
 @onready var button: Button = $HBoxContainer/Button
 
 @export var action_name : String = "move_left"
+var previous_button = null
 
 func _ready() -> void:
 	set_process_unhandled_key_input(false)
 	set_action_name()
 	set_text_for_key()
-	
+
 func set_action_name() -> void:
 	label.text = "Unassigned"
 	
@@ -28,39 +29,47 @@ func set_action_name() -> void:
 
 func set_text_for_key() -> void:
 	var action_events = InputMap.action_get_events(action_name)
-	var action_event= action_events[0]
-	var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
-	
-	button.text = "%s" % action_keycode
-	
-	
-	
+	if action_events.size() > 0:
+		var action_event = action_events[0]
+		var action_keycode = OS.get_keycode_string(action_event.physical_keycode)
+		button.text = "%s" % action_keycode
+	else:
+		button.text = "Unassigned"
+
 func _on_button_toggled(button_pressed) -> void:
 	if button_pressed:
 		button.text = "Press any key..."
 		set_process_unhandled_key_input(button_pressed)
-		
-		for i in get_tree().get_nodes_in_group("hotkey_button"):
-			if i.action_name != self.action_name:
-				i.button.roggle_mode =false
-				i.set_process_unhandled_key_input(false)
+
+		if previous_button != null and previous_button != self:
+			previous_button.button.toggle_mode = false
+			previous_button.set_process_unhandled_key_input(false)
+
+		previous_button = self
 	else:
-		for i in get_tree().get_nodes_in_group("hotkey_button"):
-			if i.action_name != self.action_name:
-				i.button.roggle_mode = true
-				i.set_process_unhandled_key_input(false)
+		button.toggle_mode = true
+		set_process_unhandled_key_input(false)
 		set_text_for_key()
 
-
 func _unhandled_key_input(event: InputEvent) -> void:
-	rebind_action_key(event)
-	button.button_pressed=false
-	
+	if event is InputEventKey:
+		var already_bound = is_key_already_bound(event)
+		if not already_bound:
+			rebind_action_key(event)
+		button.button_pressed = false
+
+func is_key_already_bound(event: InputEventKey) -> bool:
+	for action in InputMap.get_actions():
+		var action_events = InputMap.action_get_events(action)
+		for action_event in action_events:
+			if action_event is InputEventKey and action_event.physical_keycode == event.physical_keycode:
+				return true
+	return false
+
 func rebind_action_key(event) -> void:
 	InputMap.action_erase_events(action_name)
-	InputMap.action_add_event(action_name,event)
-	
+	InputMap.action_add_event(action_name, event)
+
 	set_process_unhandled_key_input(false)
 	set_text_for_key()
 	set_action_name()
-	
